efe22fdbe6422e53df6f1228768b0006365d5fe6
wubei <5306546@qq.com>
2015-08-29 02:07:54 +0800

    添加fastjson依赖，配置返回json

--- src/main/java/com/rapid/common/base/controller/BaseController.java
+++ src/main/java/com/rapid/common/base/controller/BaseController.java
@@ -6,15 +6,18 @@ import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * BaseController是所有控制器的基类
- * 封装了常用返回的方法
+ * BaseController是所有控制器的基类,封装了常用返回的方法<br/>
+ * 当返回JSON格式对象时,直接返回Object对象即可<br/>
+ * 例:	@ResponseBody public Object json() { return obj; }<br/>
+ * 返回的obj对象可以是List,Map,或一个实体
+ * 
  * @author wubei
  *
  */
 public class BaseController {
 	
 	protected final Logger logger = Logger.getLogger(getClass());
-
+	
 	protected ModelAndView render(String viewName) {
 		return new ModelAndView(viewName);
 	}
--- src/main/java/com/rapid/module/party/controller/IndexController.java
+++ src/main/java/com/rapid/module/party/controller/IndexController.java
@@ -1,13 +1,11 @@
 package com.rapid.module.party.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONObject;
 import com.rapid.common.base.controller.BaseController;
 import com.rapid.common.plugin.config.Config;
 import com.rapid.common.plugin.config.SystemGlobal;
@@ -27,10 +25,11 @@ public class IndexController extends BaseController {
 
 	@RequestMapping("/test")
 	@ResponseBody
-	public Map<String, Object> test() {
-		Map<String,Object> map = new HashMap<String,Object>();  
-        map.put("code", true);
-		return map;
+	public Object test() {
+		JSONObject json = new JSONObject();
+		json.put("11", "22222");
+        
+		return json;
 	}
 	
 }
